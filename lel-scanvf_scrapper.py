eccbb70c04a3ce39dbf016d9d8385d51cb0134fd
Sébastien AMOURY <pour4shared@hotmail.fr>
2020-12-03 16:24:04 +0100

    Adding downloading an entire serie feature

--- lel-scanvf_scrapper.py
+++ lel-scanvf_scrapper.py
@@ -3,61 +3,55 @@ import requests
 import shutil
 import os
 
+
+
 def get_img_url_from_page(url):
     page = requests.get(url)
     soup = BS(page.content,'html.parser')
 
     image= soup.find('img',attrs={'class':'img-responsive scan-page'})
-    image=image['src'].split(" ")[1]
+    img=""
+    if image!=None:
+        img=image['src'].split(" ")[1]
 
-    print(image)
-    return image
+    #print(img)
+    return img
 
-
-
-def dl_image_from_src(url):
+def dl_image_from_src(url,path):
     ## Set up the image URL and filename
     image_url = get_img_url_from_page(url)
-    filename = image_url.split("/")[-1]
-    chapter=image_url.split("/")[-2]
+    if image_url!="":
+        filename = image_url.split("/")[-1]
 
-    path=chapter+"/"+filename
-    # toCreate = os.getcwd()+"/"+chapter
-    # print(toCreate)
-    # try:
-    #     os.mkdir(toCreate)
-    # except OSError:
-    #     print("Impossible de créer le dossier")
-    # else:
-    #     print("Création dossier : %s"%toCreate)
+        path+="/"+filename
+        # Open the url image, set stream to True, this will return the stream content.
+        r = requests.get(image_url, stream=True)
 
-    # Open the url image, set stream to True, this will return the stream content.
-    r = requests.get(image_url, stream=True)
+        # Check if the image was retrieved successfully
+        if r.status_code == 200:
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            r.raw.decode_content = True
 
-    # Check if the image was retrieved successfully
-    if r.status_code == 200:
-        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-        r.raw.decode_content = True
+            # Open a local file with wb ( write binary ) permission.
+            with open(path, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
 
-        # Open a local file with wb ( write binary ) permission.
-        with open(path, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)
+            print('Image sucessfully Downloaded: ', filename)
+        else:
+            print('Image Couldn\'t be retreived')
 
-        print('Image sucessfully Downloaded: ', filename)
-    else:
-        print('Image Couldn\'t be retreived')
-
-def dl_chapter():
-
-
-    chapter_to_dl=str(input("Entrez l'url du chapitre lelscan a dl :\n"))
+def dl_chapter(chapter_to_dl):
     if chapter_to_dl[-1]!="/":
         chapter_to_dl+="/"
     ## Set up the image URL and filename
     image_url = get_img_url_from_page(chapter_to_dl)
     chapter=image_url.split("/")[-2]
-    toCreate = os.getcwd()+"/"+chapter
-    print(toCreate)
+    serie=image_url.split("/")[-4]
+
+    #create_serie_rep(os.getcwd()+"/"+serie)
+
+    toCreate = os.getcwd()+"/"+serie+"/"+chapter
+    #print(toCreate)
     try:
         os.mkdir(toCreate)
     except OSError:
@@ -65,8 +59,38 @@ def dl_chapter():
     else:
         print("Création dossier : %s"%toCreate)
 
-    for i in range(1,100):
+    for i in range(1,30):
         url=chapter_to_dl+str(i)
-        dl_image_from_src(url)
+        r = requests.get(url)
+        if r.status_code==200:
+            dl_image_from_src(url,toCreate)
+        else:
+            break
 
-dl_chapter()
+def create_serie_rep(path):
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Impossible de créer le dossier %s" %path)
+    else:
+        print("Création dossier : %s"%path)
+
+def dl_all_chapters():
+
+    series=str(input("Entrez l'url du manga lelscan a dl :\n"))
+    if series[-1]!="/":
+        series+="/" # on ajoute s'il n'y est pas
+    #print(os.getcwd()+"/"+series.split("/")[-2])
+    create_serie_rep(os.getcwd()+"/"+series.split("/")[-2])
+
+    #print(series+"1")
+    for i in range (1,300):
+        r = requests.get(series+str(i)+"/")
+        if r.status_code==200:
+            dl_chapter(series+str(i)+"/")
+        else:
+            break
+#dl_chapter()
+dl_all_chapters()
+
+#get_img_url_from_page("https://www.lelscan-vf.me/manga/solo-leveling/129/1")
